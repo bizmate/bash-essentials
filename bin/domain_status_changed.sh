033b98ac27a685284b0cc900d3b58d26973d28a7
bizmate <diego_gullo@bizmate.biz>
2019-10-08 10:32:21 +0100

    Domain status change allow overriding of run times and add some verbosing

--- bin/domain_status_changed.sh
+++ bin/domain_status_changed.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # depends on https://github.com/nixcraft/domain-check-2
 # uses domain file list ~/domains-check-list.txt
-HOW_MANY_TIMES_RUNS=10
+HOW_MANY_TIMES_RUNS="${HOW_MANY_TIMES_RUNS:-10}"
 
 if [ -z "${DOMAIN_SEND_TO_EMAIL}" ]  ; then
 	echo "Please set DOMAIN_SEND_TO_EMAIL";
@@ -15,6 +15,10 @@ fi
 
 for ((i=0; i<HOW_MANY_TIMES_RUNS; i++))
 do
+	echo "Running $i of $HOW_MANY_TIMES_RUNS"
+	date
 	DOMAIN_STATUS_CHANGED="$( ./domain-check-2.sh -f ~/domains-check-list.txt | grep -v 'Expired\|Status\|-----------------------' )" && [ -n "$DOMAIN_STATUS_CHANGED" ] && echo "$DOMAIN_STATUS_CHANGED" | mailx -v -s "Domain_check_email-Bizmate" $DOMAIN_SEND_TO_EMAIL
 	sleep $SLEEP_INTERVAL
 done
+
+echo Finished
