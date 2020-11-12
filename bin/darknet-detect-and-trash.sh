b190cfc5edc10ec1cdc7e7b735a98910c1996ec3
bizmate <diego_gullo@bizmate.biz>
2020-11-12 00:13:26 +0000

    Modify script so it can run scanning of files under all subfolders.

--- bin/darknet-detect-and-trash.sh
+++ bin/darknet-detect-and-trash.sh
@@ -40,15 +40,17 @@ export mediaFolder
 #processing function
 checkAndTrash() {
 	extension="${1##*.}"
+	filePath=$( dirname "$1" )
+	fileName=$( basename "$1" )
 	# filename="${1%.*}"
-	echo "checking file $1 with extension $extension under folder $mediaFolder"
+	echo "checking path $filePath fileName $fileName file $1 with extension $extension under folder $mediaFolder"
 
-	if [[ -s $mediaFolder/$1 ]];
+	if [[ -s $filePath/$fileName ]];
 	then
 		echo "file has something";
 	else
-		echo "$1 is empty, deleting and  returning 0"
-		gvfs-trash "$mediaFolder/$1"
+		echo "$fileName is empty, deleting and  returning 0"
+		gvfs-trash "$filePath/$fileName"
 		return 0
 	fi
 
@@ -60,22 +62,22 @@ checkAndTrash() {
 		echo "FPS Count $fpsCount"
 		if [[ "$fpsCount" -gt "0" ]];
 		then
-			echo "$1 was already checked, returning 0"
+			echo "$fileName was already checked, returning 0"
 			return 0
 		else
-			echo "$1 was not checked"
+			echo "$fileName was not checked"
 		fi
 	fi
 
 	if [[ "$extension" == 'jpg' ]]
 	then
 		docker run --gpus all --rm -v "$DARKNET_WORKSPACE_FOLDER":/workspace -v "$YOLO_LIB_FOLDER":/yolo-lib \
-		-v "$mediaFolder":/images -w /workspace daisukekobayashi/darknet:gpu-cv darknet detector test data/coco.data \
-		/yolo-lib/yolov3.cfg /yolo-lib/yolov3.weights  "/images/$1" -dont_show -ext_output > "$1.log"
+		-v "$filePath":/images -w /workspace daisukekobayashi/darknet:gpu-cv darknet detector test data/coco.data \
+		/yolo-lib/yolov3.cfg /yolo-lib/yolov3.weights  "/images/$fileName" -dont_show -ext_output > "$1.log"
 	else
 		docker run --gpus all --rm -v "$DARKNET_WORKSPACE_FOLDER":/workspace -v "$YOLO_LIB_FOLDER":/yolo-lib \
-		-v "$mediaFolder":/images -w /workspace daisukekobayashi/darknet:gpu-cv darknet detector demo data/coco.data \
-		/yolo-lib/yolov3.cfg /yolo-lib/yolov3.weights  "/images/$1" -dont_show -ext_output | awk '/Objects:/,/FPS/' \
+		-v "$filePath":/images -w /workspace daisukekobayashi/darknet:gpu-cv darknet detector demo data/coco.data \
+		/yolo-lib/yolov3.cfg /yolo-lib/yolov3.weights  "/images/$fileName" -dont_show -ext_output | awk '/Objects:/,/FPS/' \
 		> "$1.log"
 	fi
 
@@ -92,8 +94,8 @@ checkAndTrash() {
 		personCount=$(grep -c person "$1.log")
 		if [[ "$personCount" -eq "0" ]];
 		then
-		   echo "moving $mediaFolder/$1 to trash";
-		   gvfs-trash "$mediaFolder/$1"
+		   echo "moving $filePath/$fileName to trash";
+		   gvfs-trash "$filePath/$fileName"
 		fi
 	else
 		echo "broken detection for file $1.log"
@@ -102,4 +104,4 @@ checkAndTrash() {
 
 export -f checkAndTrash
 
-find "$mediaFolder" -type f -regex "$videoAndImageFiles" -execdir bash -c 'checkAndTrash "$0"' {} \;
+find "$mediaFolder" -type f -regex "$videoAndImageFiles" -exec bash -c 'checkAndTrash "$0"' {} \;
